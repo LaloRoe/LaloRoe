9c4ffb37b11e9fbcdbd34f4ff8868bc6ea5bc18e
Eduardo Ortiz Romero <mrlalo_roe@outlook.com>
2023-09-16 20:08:50 -0600

    nuevo

--- content/config.mjs
+++ content/config.mjs
@@ -1,1 +1,17 @@
-export default {};
+//export default {};
+const config = {
+    // title will be displayed on the top of your site
+    title: "LaloRoe.com",
+    // adding a description helps with SEO
+    description: "Mi blog personal y profesional",
+    // author of site displayed on the bottom of your site
+    //author: "John",
+    // logo image
+    //authorLogo: "/images/logo.svg",
+    // url to author website
+    //domain: "https://john.app/",
+    // links to the pages you want to link to in the navbar
+    navLinks: [{ href: "/perfil", name: "Perfil" },{ href: "/notas", name: "Mis Notas" },{ href: "/DS_IA", name: "Data Science e Inteligencia Artificial" },{ href: "/mas", name: "Más" },{ href: "/blog", name: "Blog" }],
+  };
+  
+  export default config;
