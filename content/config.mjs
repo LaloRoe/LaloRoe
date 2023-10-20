09ffb52978abcfb884c9e1ee218012e341bd9765
Eduardo Ortiz Romero <74125420+LaloRoe@users.noreply.github.com>
2023-10-19 23:15:26 -0600

    Update config.mjs

--- content/config.mjs
+++ content/config.mjs
@@ -6,6 +6,7 @@ const config = {
     description: "Mi Notas de Formación Profecional",
     // author of site displayed on the bottom of your site
     author: "laloroe",
+    showSidebar: true,
     showComments: true,
     comments: {
         provider: "utterances",
